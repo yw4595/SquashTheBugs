056727399a6e9f48dd62d3330390cae3b451186b
Yanzhi Wang <yw4595@g.rit.edu>
2023-01-24 13:05:47 +0300

    Bug squashing exercise

--- Program.cs
+++ Program.cs
@@ -1,46 +1,56 @@
 ﻿using System;
 
-namespace SquashTheBugs2._0
+namespace SquashTheBugs
 {
-    /*
-    * Class Program
-    * Author: Yanzhi Wang
-    * Purpose: Bug squashing exercise
-    * Restrictions: None
-    */
-    static class Program
+    // Class Program
+    // Author: David Schuh
+    // Purpose: Bug squashing exercise
+    // Restrictions: None
+    class Program
     {
-        /*
-        * Method: Main
-        * Purpose: Loop through the numbers 1 through 10 
-        *          Output N/(N-1) for all 10 numbers with 5 decimal places
-        *          and list all numbers processed
-        * Restrictions: None
-        */
+        private static string allNumbers;
+
+        // Method: Main
+        // Purpose: Loop through the numbers 1 through 10
+        // Output N/(N-1) for all 10 numbers
+        // and list all numbers processed
+        // Restrictions: None
         static void Main(string[] args)
         {
             // declare int counter
-            int i = 0;
-            // declare string to hold all numbers
-            string allNumbers = null;
-            // loop through the numbers 1 through 10
-            for (i = 1; i <= 10; ++i)
+            int i = 0; // missing semicolon, compile-time error
+                       // loop through the numbers 1 through 10
+            for (i = 1; i <= 10; ++i) // changed < to <= to include 10 in the loop, logical error
             {
+                // declare string to hold all numbers
+                string allNumbers = null;
+
                 // output explanation of calculation
-                Console.Write(i + "/" + (i - 1) + " = ");
+                Console.Write(i + "/" + (i - 1) + " = "); // added parentheses to separate i and i-1, compile-time error
 
-                // output the calculation based on the numbers with 5 decimal places
-                Console.WriteLine(Math.Round(i / (double)(i - 1), 5)); //making sure that one of the argument is double
+                // added check for division by zero
+                if (i - 1 == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                }
+                else
+                {
+                    Console.WriteLine(Math.Round((double)i / (i - 1), 3)); //using Math.Round() to get 3 decimal places
+                }
 
-                // concatenate each number to allNumbers
-                allNumbers += i + " ";
-
-                // increment the counter 
-                //no need to increment the counter as its already done in the loop
+                // added check for division by zero before concatenating to allNumbers
+                if (i - 1 != 0)
+                {
+                    allNumbers += i + " ";
+                }
+                else
+                {
+                    allNumbers += i + "(division by zero) ";
+                }
             }
 
             // output all numbers which have been processed
-            Console.WriteLine("These numbers have been processed: " + allNumbers);
+            Console.WriteLine("These numbers have been processed: " + allNumbers); // added missing + operator, compile-time error
         }
     }
 }
